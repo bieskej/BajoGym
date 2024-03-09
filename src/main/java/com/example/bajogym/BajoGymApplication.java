f93ff683ce75f5983b5cf4250c387e7199d4aca8
bieskej <bosko.raguz@fsre.sum.ba>
2024-03-09 22:00:25 +0100

    Initial commit

---
+++ src/main/java/com/example/bajogym/BajoGymApplication.java
@@ -0,0 +1,13 @@
+package com.example.bajogym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BajoGymApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BajoGymApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bajogym/BajoGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bajogym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BajoGymApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
